46069b7903db2c2ca1ab180831c5ce08af03aaec
Harshgp44 <harshgp44@gmail.com>
2018-04-17 22:13:30 +0530

    added all

--- news_websites.py
+++ news_websites.py
@@ -3,22 +3,21 @@ import json
 app = Flask(__name__)
 import time
 import thread
-topics=['json/top','json/business','json/sports','json/tech','json/entertainment']
+topics=['json/top','json/business','json/sports','json/tech','json/entertainment','json/politics','json/world']
 TEMPLATES_AUTO_RELOAD = True
 app.config['DEBUG'] = True
 
 
 content=[]
-def looping():
-    for i in topics:
-         f=open(str(i)+".json",'r')
+def looping(index):
+
+         f=open(str(topics[index])+".json",'r')
          data=json.load(f)
          # json.dump(top20)
-         content.append(data)
-         print(content)
+         print(data)
          #print(content['headlines']['articles'][0]['title'])
-    # raw_input()
-    return content
+        # raw_input()
+         return data
 
 
 
@@ -28,9 +27,39 @@ def main():
     return render_template('index.html')
 
 
-@app.route('/data',methods=['POST','GET'])
-def data():
-    data=looping()
+@app.route('/top',methods=['POST','GET'])
+def top():
+    data=looping(0)
+    return jsonify(data)
+
+@app.route('/ent',methods=['POST','GET'])
+def ent():
+    data=looping(4)
+    return jsonify(data)
+
+@app.route('/spo',methods=['POST','GET'])
+def spo():
+    data=looping(2)
+    return jsonify(data)
+
+@app.route('/tec',methods=['POST','GET'])
+def tec():
+    data=looping(3)
+    return jsonify(data)
+
+@app.route('/pol',methods=['POST','GET'])
+def pol():
+    data=looping(5)
+    return jsonify(data)
+
+@app.route('/world',methods=['POST','GET'])
+def world():
+    data=looping(6)
+    return jsonify(data)
+
+@app.route('/bus',methods=['POST','GET'])
+def bus():
+    data=looping(1)
     return jsonify(data)
 
 @app.route('/page',methods=['GET'])
--- scraping_data.py
+++ scraping_data.py
@@ -3,9 +3,39 @@ import urllib2
 import json
 from bs4 import BeautifulSoup
 
-response=urllib2.urlopen('http://news.google.com/news/headlines/section/topic/POLITICS.en_in/Politics?ned=in&hl=en-IN&gl=IN')
-soup=BeautifulSoup(response,"html.parser")
-for i in soup.find_all('a',attrs={'class':"nuEeue hzdq5d ME7ew"}):
-    print(i.text)
+politics="http://news.google.com/news/headlines/section/topic/POLITICS.en_in/Politics?ned=in&hl=en-IN&gl=IN"
+world="https://news.google.com/news/headlines/section/topic/WORLD.en_in/World?ned=in&hl=en-IN&gl=IN"
 
+urls=[politics,world]
+# for j in urls:
+
+for j in urls:
+    list=[]
+    response=urllib2.urlopen(j)
+    soup=BeautifulSoup(response,"html.parser")
+    for i in soup.find_all('div',attrs={'class':'qx0yFc'}):
+
+
+    # image=i.find('a',attts={'class':'MWG8ab'})
+    # print(image)
+    # print(image.get('href'))
+    # exact_img=image.find('img',attrs={'class':'lmFAjc'})
+    # print(exact_img.get('src'))
+
+
+
+        content=i.find('a',attrs={'class':"nuEeue hzdq5d ME7ew"})
+        # print(content)
+        try:
+            image=i.img['src']
+            list.append([content.text,content.get('href'),i.img['src']])
+        except:
+            list.append([content.text,content.get('href')])
+    print(list)
+    if(j==politics):
+        f=open('json/politics.json','w')
+        json.dump(list,f)
+    elif(j==world):
+        f=open('json/world.json','w')
+        json.dump(list,f)
 
