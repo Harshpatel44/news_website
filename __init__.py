6e2f0b9fce997be9ef7a11cbec9ddcaa3ead9f78
Harshgp44 <harshgp44@gmail.com>
2018-04-24 21:13:49 +0530

    updated till now

--- __init__.py
+++ __init__.py
@@ -1,66 +1,99 @@
-from flask import *
+from flask import render_template,Flask,jsonify
 import json
 app = Flask(__name__)
+import content
 import time
 import thread
-topics=['json/top','json/business','json/sports','json/tech','json/entertainment','json/politics','json/world']
+topics=['json/top','json/business','json/sports','json/tech','json/entertainment','json/politics','json/world','json/finance']
 TEMPLATES_AUTO_RELOAD = True
 app.config['DEBUG'] = True
 
 
-content=[]
-def looping(index):
 
-         f=open(str(topics[index])+".json",'r')
+def looping():     #continues in interval of 30s
+     all_data2=[]
+     # content.scrap_data()
+     time.sleep(0.2)
+     for i in topics:
+         f=open(str(i)+".json",'r')
          data=json.load(f)
-         # json.dump(top20)
-         # print(data)
-         #print(content['headlines']['articles'][0]['title'])
-        # raw_input()
-         return data
+         content.scrap_data()
+         all_data2.append(data)
+
+         f.close()
+     return all_data2
+
+def first_time():  #for the first time when website loads
+    all_data=[]
+    for i in topics:
+         time.sleep(0.1)
+         f=open(str(i)+".json",'r')
+
+         data=json.load(f)
+
+         all_data.append(data)
+
+         f.close()
+    return all_data
 
 
+@app.route('/responsive')
+def responsive():
+    return render_template('responsive.html')
 
 
 @app.route('/',methods=['POST','GET'])
 def main():
+    print('main')
+
     return render_template('index.html')
 
+@app.route('/first',methods=['GET','POST'])
+def first():
+    data=first_time()
+    return jsonify(data)
 
 @app.route('/top',methods=['POST','GET'])
 def top():
-    data=looping(0)
+    data=looping()
     return jsonify(data)
 
-@app.route('/ent',methods=['POST','GET'])
-def ent():
-    data=looping(4)
-    return jsonify(data)
-
-@app.route('/spo',methods=['POST','GET'])
-def spo():
-    data=looping(2)
-    return jsonify(data)
-
-@app.route('/tec',methods=['POST','GET'])
-def tec():
-    data=looping(3)
-    return jsonify(data)
-
-@app.route('/pol',methods=['POST','GET'])
-def pol():
-    data=looping(5)
-    return jsonify(data)
-
-@app.route('/world',methods=['POST','GET'])
-def world():
-    data=looping(6)
-    return jsonify(data)
-
-@app.route('/bus',methods=['POST','GET'])
-def bus():
-    data=looping(1)
-    return jsonify(data)
+# @app.route('/ent',methods=['POST','GET'])
+# def ent():
+#     print('ent')
+#     data=looping(4)
+#     return jsonify(data)
+#
+# @app.route('/spo',methods=['POST','GET'])
+# def spo():
+#     print('spo')
+#     data=looping(2)
+#     return jsonify(data)
+#
+# @app.route('/tec',methods=['POST','GET'])
+# def tec():
+#     print('tec')
+#     data=looping(3)
+#     return jsonify(data)
+#
+# @app.route('/pol',methods=['POST','GET'])
+# def pol():
+#     print('pol')
+#     data=looping(5)
+#     return jsonify(data)
+#
+# @app.route('/world',methods=['POST','GET'])
+# def world():
+#     print('world')
+#     data=looping(6)
+#     return jsonify(data)
+#
+# @app.route('/bus',methods=['POST','GET'])
+# def bus():
+#     print('bus')
+#     data=looping(1)
+#     return jsonify(data)
+#
 
 @app.route('/page',methods=['GET'])
 def page():
--- content.py
+++ content.py
@@ -1,5 +1,5 @@
 __author__ = 'harsh'
-__author__ = 'harsh'
+
 import urllib2
 import json
 import time
@@ -13,71 +13,81 @@ sports_url="http://newsapi.org/v2/top-headlines?country=in&category=sports&apiKe
 tech_url="http://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=0fc0e9b54d5d4389b807d66a86fcee50"
 entertainment_url="http://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=0fc0e9b54d5d4389b807d66a86fcee50"
 politics="http://news.google.com/news/headlines/section/topic/POLITICS.en_in/Politics?ned=in&hl=en-IN&gl=IN"
-world="https://news.google.com/news/headlines/section/topic/WORLD.en_in/World?ned=in&hl=en-IN&gl=IN"
+world="http://news.google.com/news/headlines/section/topic/WORLD.en_in/World?ned=in&hl=en-IN&gl=IN"
+finance="http://newsapi.org/v2/top-headlines?sources=financial-times&apiKey=0fc0e9b54d5d4389b807d66a86fcee50"
 
 topics=[]
-topics.extend((the_hindu_url,business_url,sports_url,tech_url,entertainment_url,politics,world))
-while(1):
+topics.extend((the_hindu_url,business_url,sports_url,tech_url,entertainment_url,politics,world,finance))
+def scrap_data():
 
     for i in topics:
-        data=urllib2.urlopen(i)
+
+            data=urllib2.urlopen(i)
 
 
-        if(i==the_hindu_url):
-            json_data=json.load(data)
-            #content['top']=json_data
-            f=open('json/top.json','w')
-            json.dump(json_data,f)
-        elif(i==business_url):
-            json_data=json.load(data)
-            # content['business']=json_data
-            f=open('json/business.json','w')
-            json.dump(json_data,f)
-        elif(i==sports_url):
-            json_data=json.load(data)
-            # content['sports']=json_data
-            f=open('json/sports.json','w')
-            json.dump(json_data,f)
-        elif(i==tech_url):
-            json_data=json.load(data)
-            # content['tech']=json_data
-            f=open('json/tech.json','w')
-            json.dump(json_data,f)
-        elif(i==entertainment_url):
-            json_data=json.load(data)
-            # content['entertainment']=json_data
-            f=open('json/entertainment.json','w')
-            json.dump(json_data,f)
-        elif(i==politics or i==world):
-            list=[]
-            response=urllib2.urlopen(i)
-            soup=BeautifulSoup(response,"html.parser")
-            for j in soup.find_all('div',attrs={'class':'qx0yFc'}):
+            if(i==the_hindu_url):
+                json_data=json.load(data)
+                #content['top']=json_data
+                f=open('top.json','w')
+                json.dump(json_data,f)
+            elif(i==business_url):
+                json_data=json.load(data)
+                # content['business']=json_data
+                f=open('json/business.json','w')
+                json.dump(json_data,f)
+            elif(i==sports_url):
+                json_data=json.load(data)
+                # content['sports']=json_data
+                f=open('json/sports.json','w')
+                json.dump(json_data,f)
+            elif(i==tech_url):
+                json_data=json.load(data)
+                # content['tech']=json_data
+                f=open('json/tech.json','w')
+                json.dump(json_data,f)
+            elif(i==entertainment_url):
+                json_data=json.load(data)
+                # content['entertainment']=json_data
+                f=open('json/entertainment.json','w')
+                json.dump(json_data,f)
+            elif(i==finance):
+                json_data=json.load(data)
+                # content['entertainment']=json_data
+                f=open('json/finance.json','w')
+                json.dump(json_data,f)
+            # elif(i==finance):
+            #     json_data=json.load(data)
+            #     f=open('json/finance.json','w')
+            elif(i==politics or i==world):
+                list=[]
+                response=urllib2.urlopen(i)
+                soup=BeautifulSoup(response,"html.parser")
+                for j in soup.find_all('div',attrs={'class':'qx0yFc'}):
 
 
-            # image=i.find('a',attts={'class':'MWG8ab'})
-            # print(image)
-            # print(image.get('href'))
-            # exact_img=image.find('img',attrs={'class':'lmFAjc'})
-            # print(exact_img.get('src'))
+                # image=i.find('a',attts={'class':'MWG8ab'})
+                # print(image)
+                # print(image.get('href'))
+                # exact_img=image.find('img',attrs={'class':'lmFAjc'})
+                # print(exact_img.get('src'))
 
 
 
-                content=j.find('a',attrs={'class':"nuEeue hzdq5d ME7ew"})
-                # print(content)
-                try:
-                    image=j.img['src']
-                    list.append([content.text,content.get('href'),j.img['src']])
-                except:
-                    list.append([content.text,content.get('href')])
-            # print(list)
-            if(i==politics):
-                f=open('json/politics.json','w')
-                json.dump(list,f)
-            elif(i==world):
-                f=open('json/world.json','w')
-                json.dump(list,f)
-    time.sleep(30)
+                    content=j.find('a',attrs={'class':"nuEeue hzdq5d ME7ew"})
+                    # print(content)
+                    try:
+                        image=j.img['src']
+                        list.append([content.text,content.get('href'),j.img['src']])
+                    except:
+                        list.append([content.text,content.get('href')])
+                # print(list)
+                if(i==politics):
+                    f=open('json/politics.json','w')
+                    json.dump(list,f)
+                elif(i==world):
+                    f=open('json/world.json','w')
+                    json.dump(list,f)
+
 
     #print(json_data)
 # print(content[['articles'][0]['title'])  #to access the content
